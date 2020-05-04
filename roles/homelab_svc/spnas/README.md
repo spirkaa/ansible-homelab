cfbeb23bfb9ec3f3c30b89c04c375fca45fa49f6
spirkaa <spirkaa@gmail.com>
2020-05-04 23:22:57 +0300

    spnas: added role

---
+++ roles/homelab_svc/spnas/README.md
@@ -0,0 +1,17 @@
+Роль Ansible: SPNAS
+===
+
+Роль Ansible, которая запускает службу полуавтоматической загрузки торрентов и доступа к файлам по протоколу SMB в составе Docker контейнеров:
+  * Deluge
+  * Sonarr
+  * Radarr
+  * Lidarr
+  * Bazarr
+  * Jackett
+  * Monitorrent
+  * Muximux
+  * Samba
+
+Зависимости
+---
+Нет
---
+++ roles/homelab_svc/spnas/files/settings.ini.php
@@ -0,0 +1,149 @@
+; <?php die('Access denied'); ?>
+[general]
+title = "SPNAS"
+branch = "master"
+theme = "modern"
+color = "#31ac63"
+mobileoverride = "true"
+tabcolor = "true"
+rssUrl = "https://www.wired.com/feed/"
+userNameInput = "admin"
+password = "muximux"
+
+[settings]
+branch_changed = ""
+sha = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+branch = "HEAD"
+branches[develop] = "92b392c88b24faa6dd63c9ada2ee7db6b667e123"
+branches[experimental] = "262b1a4c4ca6fa0386e46400211604d66c0bd22d"
+branches[master] = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+branches[parseError] = "14ea3581389e329e27c496367b2b85b15fc106f9"
+branches[saveProblem] = "4d16f3f4b1d7fb18bac5d8ed09b9a45dbe6711ff"
+last_check = 1588513166
+
+[Plex]
+name = "Plex"
+url = "http://plex:32400/web/index.html"
+scale = 1
+icon = "muximux-plex"
+color = "#cc7b19"
+enabled = "true"
+landingpage = "true"
+default = "true"
+
+[PlexPy]
+name = "Tautulli"
+url = "http://plex:8181/"
+scale = 1
+icon = "muximux-plexivity"
+color = "#f9be03"
+enabled = "true"
+
+[498083newApplication]
+name = "Ombi"
+url = "http://plex:3579/"
+scale = 1
+icon = "muximux-search"
+color = "#df691a"
+enabled = "true"
+
+[deluge]
+name = "Deluge"
+url = "http://spnas:8112/"
+scale = 1
+icon = "muximux-deluge"
+color = "#3d85c6"
+enabled = "true"
+
+[Sonarr]
+name = "Sonarr"
+url = "http://spnas:8989/"
+scale = 1
+icon = "muximux-sonarr"
+color = "#2193b5"
+enabled = "true"
+
+[616013newApplication]
+name = "Radarr"
+url = "http://spnas:7878/"
+scale = 1
+icon = "muximux-sonarr"
+color = "#ffc230"
+enabled = "true"
+
+[789499newApplication]
+name = "Lidarr"
+url = "http://spnas:8686/"
+scale = 1
+icon = "muximux-library_music"
+color = "#00a65b"
+enabled = "true"
+dd = "true"
+
+[852017newApplication]
+name = "Bazarr"
+url = "http://spnas:6767/"
+scale = 1
+icon = "muximux-subtitles"
+color = "#cccccc"
+enabled = "true"
+dd = "true"
+
+[669967newApplication]
+name = "Jackett"
+url = "http://spnas:9117/"
+scale = 1
+icon = "muximux-jackett"
+color = "#333333"
+enabled = "true"
+
+[349196newApplication]
+name = "Portainer"
+url = "http://portainer/"
+scale = 1
+icon = "muximux-stack"
+color = "#30426a"
+enabled = "true"
+
+[255367newApplication]
+name = "Monitorrent"
+url = "http://spnas:6687/"
+scale = 1
+icon = "muximux-calendar2"
+color = "#607d8b"
+enabled = "true"
+dd = "true"
+
+[420636newApplication]
+name = "LibreNMS"
+url = "http://nms.stats/"
+scale = 1
+icon = "muximux-area-chart"
+color = ""
+enabled = "true"
+dd = "true"
+
+[909064newApplication]
+name = "Grafana"
+url = "http://grafana.stats/"
+scale = 1
+icon = "muximux-dashboard"
+color = ""
+enabled = "true"
+dd = "true"
+
+[116329newApplication]
+name = "Domoticz"
+url = "http://dom.stats/"
+scale = 1
+icon = "muximux-home3"
+color = ""
+enabled = "true"
+dd = "true"
