e5f368ee3837e4b5869ea6bc2b022c1def174972
Ilya Pavlov <piv@devmem.ru>
2020-08-02 08:27:15 +0300

    spnas: muximux: updated settings

--- roles/homelab_svc/spnas/files/settings.ini.php
+++ roles/homelab_svc/spnas/files/settings.ini.php
@@ -12,7 +12,6 @@ password = "muximux"
 
 [settings]
 branch_changed = ""
-sha = "66b11488206b44364b6ed8bb438462cbbb0835c5"
 enabled = "true"
 default = "false"
 name = "Settings"
@@ -20,17 +19,10 @@ url = "muximux.php"
 landingpage = "false"
 icon = "fa-cog"
 dd = "true"
-branch = "HEAD"
-branches[develop] = "92b392c88b24faa6dd63c9ada2ee7db6b667e123"
-branches[experimental] = "262b1a4c4ca6fa0386e46400211604d66c0bd22d"
-branches[master] = "66b11488206b44364b6ed8bb438462cbbb0835c5"
-branches[parseError] = "14ea3581389e329e27c496367b2b85b15fc106f9"
-branches[saveProblem] = "4d16f3f4b1d7fb18bac5d8ed09b9a45dbe6711ff"
-last_check = 1588513166
 
 [Plex]
 name = "Plex"
-url = "http://plex:32400/web/index.html"
+url = "https://plex.devmem.ru/web/index.html"
 scale = 1
 icon = "muximux-plex"
 color = "#cc7b19"
@@ -38,25 +30,25 @@ enabled = "true"
 landingpage = "true"
 default = "true"
 
-[PlexPy]
+[Tautulli]
 name = "Tautulli"
-url = "http://plex:8181/"
+url = "https://tautulli.devmem.ru/"
 scale = 1
 icon = "muximux-plexivity"
 color = "#f9be03"
 enabled = "true"
 
-[498083newApplication]
+[Ombi]
 name = "Ombi"
-url = "http://plex:3579/"
+url = "https://ombi.devmem.ru/"
 scale = 1
 icon = "muximux-search"
 color = "#df691a"
 enabled = "true"
 
-[deluge]
+[Deluge]
 name = "Deluge"
-url = "http://spnas:8112/"
+url = "https://deluge.home.devmem.ru/"
 scale = 1
 icon = "muximux-deluge"
 color = "#3d85c6"
@@ -64,86 +56,51 @@ enabled = "true"
 
 [Sonarr]
 name = "Sonarr"
-url = "http://spnas:8989/"
+url = "https://sonarr.home.devmem.ru/"
 scale = 1
 icon = "muximux-sonarr"
 color = "#2193b5"
 enabled = "true"
 
-[616013newApplication]
+[Radarr]
 name = "Radarr"
-url = "http://spnas:7878/"
+url = "https://radarr.home.devmem.ru/"
 scale = 1
 icon = "muximux-sonarr"
 color = "#ffc230"
 enabled = "true"
 
-[789499newApplication]
+[Jackett]
+name = "Jackett"
+url = "https://jackett.home.devmem.ru/"
+scale = 1
+icon = "muximux-jackett"
+color = "#333333"
+enabled = "true"
+
+[Lidarr]
 name = "Lidarr"
-url = "http://spnas:8686/"
+url = "https://lidarr.home.devmem.ru/"
 scale = 1
 icon = "muximux-library_music"
 color = "#00a65b"
 enabled = "true"
 dd = "true"
 
-[852017newApplication]
+[Bazarr]
 name = "Bazarr"
-url = "http://spnas:6767/"
+url = "https://bazarr.home.devmem.ru/"
 scale = 1
 icon = "muximux-subtitles"
 color = "#cccccc"
 enabled = "true"
 dd = "true"
 
-[669967newApplication]
-name = "Jackett"
-url = "http://spnas:9117/"
-scale = 1
-icon = "muximux-jackett"
-color = "#333333"
-enabled = "true"
-
-[349196newApplication]
-name = "Portainer"
-url = "http://portainer/"
-scale = 1
-icon = "muximux-stack"
-color = "#30426a"
-enabled = "true"
-
-[255367newApplication]
+[Monitorrent]
 name = "Monitorrent"
-url = "http://spnas:6687/"
+url = "https://monitorrent.home.devmem.ru/"
 scale = 1
 icon = "muximux-calendar2"
 color = "#607d8b"
-enabled = "true"
-dd = "true"
-
-[420636newApplication]
-name = "LibreNMS"
-url = "http://nms.stats/"
-scale = 1
-icon = "muximux-area-chart"
-color = ""
-enabled = "true"
-dd = "true"
-
-[909064newApplication]
-name = "Grafana"
-url = "http://grafana.stats/"
-scale = 1
-icon = "muximux-dashboard"
-color = ""
-enabled = "true"
-dd = "true"
-
-[116329newApplication]
-name = "Domoticz"
-url = "http://dom.stats/"
-scale = 1
-icon = "muximux-home3"
-color = ""
 enabled = "true"
 dd = "true"
