5f059f41aea83d3291a699c969d59e8e70d66f59
Ilya Pavlov <piv@devmem.ru>
2022-01-29 14:32:13 +0300

    list_updated_tags: added login function

--- list_updated_tags.py
+++ list_updated_tags.py
@@ -9,14 +9,22 @@
 
 import logging
 import json
+import os
 import re
 import subprocess
-
 from datetime import datetime, timedelta
 from dateutil import parser
 from pathlib import Path
 
+import pexpect
+from dotenv import load_dotenv
+
 logger = logging.getLogger("__name__")
+
+load_dotenv()
+
+DOCKER_USERNAME = os.getenv("DOCKER_USERNAME")
+DOCKER_PASSWORD = os.getenv("DOCKER_PASSWORD")
 
 
 def get_repos(file_path: str) -> list:
@@ -27,45 +35,61 @@ def get_repos(file_path: str) -> list:
     return re.findall(r"\[(.+?)\]", content)
 
 
+def docker_login(username: str, password: str):
+    """
+    Login to the Docker Hub
+    """
+    child = pexpect.spawn("hub-tool login")
+    child.expect("Username:")
+    child.sendline(username)
+    child.expect("Password:")
+    child.sendline(password)
+    child.expect("Login Succeeded", timeout=10)
+
+
 def list_updated_tags(repository: str, days: int = 7):
     """
-    List all tags in repo with hub-tool and filter results by date
+    List all tags in repo and filter results by date
     """
-    logger.info("%s", repository)
-    hub_tool = subprocess.run(
-        [
-            "hub-tool",
-            "tag",
-            "ls",
-            "--format",
-            "json",
-            "--sort",
-            "updated",
-            repository,
-        ],
-        capture_output=True,
-        encoding="utf-8",
-    )
-
-    if hub_tool.stdout:
-        if hub_tool.stdout != "null\n":
-            for tag in json.loads(hub_tool.stdout):
-                tag_last_updated = tag["LastUpdated"]
-                tag_last_updated_dt = parser.parse(tag_last_updated).replace(
-                    tzinfo=None
-                )
-                if tag_last_updated_dt > datetime.now() - timedelta(days=days):
-                    tag_name = tag["Name"]
-                    if not any(platform in tag_name for platform in ["arm32", "arm64"]):
-                        print(tag_name, "-", tag_last_updated)
+    try:
+        logger.info("%s", repository)
+        hub_tool = subprocess.run(
+            [
+                "hub-tool",
+                "tag",
+                "ls",
+                "--format",
+                "json",
+                "--sort",
+                "updated",
+                repository,
+            ],
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
+        )
+        if hub_tool.stdout:
+            if hub_tool.stdout != "null\n":
+                for tag in json.loads(hub_tool.stdout):
+                    tag_last_updated = tag["LastUpdated"]
+                    tag_last_updated_dt = parser.parse(tag_last_updated).replace(
+                        tzinfo=None
+                    )
+                    if tag_last_updated_dt > datetime.now() - timedelta(days=days):
+                        tag_name = tag["Name"]
+                        if not any(platform in tag_name for platform in ["arm32", "arm64"]):
+                            print(tag_name, "-", tag_last_updated)
+            else:
+                logger.error("%s: repo not found on docker hub", repository)
         else:
-            logger.error("%s: repo not found on docker hub", repository)
-    else:
-        logger.error("%s: something bad happened", repository)
-    print("")
+            logger.error("%s: something bad happened", repository)
+        print("")
+    except subprocess.CalledProcessError as e:
+        print(e.stderr)
 
 
 def main():
+    docker_login(DOCKER_USERNAME, DOCKER_PASSWORD)
     repos = get_repos("roles/homelab_svc/spnas/README.md")
     for repo in repos:
         list_updated_tags(repo)
