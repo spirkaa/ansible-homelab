e40bfdc51cd91df0465d5167473cdfde72892132
Ilya Pavlov <piv@devmem.ru>
2022-03-13 21:22:06 +0300

    vps: changed antifilter list

--- roles/vps/files/antifilter.sh
+++ roles/vps/files/antifilter.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 WORKDIR=/opt/antifilter
-LIST_PATH=$WORKDIR/list
 MD5_FILE=$WORKDIR/md5.txt
+LIST_PATH=$WORKDIR/list
+LIST_NAME=allyouneed
 
-URL=https://antifilter.download/list
+URL=https://antifilter.download/list/$LIST_NAME.lst
 
 touch $MD5_FILE
 mkdir -p $LIST_PATH
 
 cd $LIST_PATH || exit
 
-wget -N $URL/ipsum.lst $URL/subnet.lst
+wget -N $URL
 
 OLD_LISTS_MD5=$(cat $MD5_FILE)
 NEW_LISTS_MD5=$(cat $LIST_PATH/*.lst | md5sum | head -c 32)
 
 if [ "$OLD_LISTS_MD5" != "$NEW_LISTS_MD5" ]; then
-  cat $LIST_PATH/ipsum.lst | sed 's_.*_route & reject;_' > /etc/bird/ipsum.txt
-  cat $LIST_PATH/subnet.lst | sed 's_.*_route & reject;_' > /etc/bird/subnet.txt
+  cat $LIST_PATH/$LIST_NAME.lst | sed 's_.*_route & reject;_' > /etc/bird/$LIST_NAME.txt
   /usr/sbin/birdc configure
   logger "antifilter lists updated"
   echo $NEW_LISTS_MD5 > $MD5_FILE
